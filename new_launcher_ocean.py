27d7a96367c37de6a494c87bff9f9af4a621d8db
Jacopo Dall'Aglio <fingolfin00@gmail.com>
2026-01-27 15:55:44 +0100

    new_launcher_ocean.py: update to support new features
    - Add start of the month flag when generating periods
    - Use ocean.juno.cmcc.hindcast provider
    - Support SST forecast experiments (6-hourly data)
    
    Signed-off-by: Jacopo Dall'Aglio <fingolfin00@gmail.com>

--- new_launcher_ocean.py
+++ new_launcher_ocean.py
@@ -7,10 +7,12 @@ from earthml.utils import halved_windows_split_by_cutoff, half_train_periods_day
 if __name__ == "__main__":
     max_retries = 4
 
-    var_exp = "sss"
+    var_exp = "t14d" # sst, sss, t14d
 
     full_leadtimes_days = (45, 75, 105, 135, 165)
     full_leadtimes_months = (1, 2, 3, 4, 5)
+    full_leadtime_hours_sst = (12, 24, 48, 72, 96, 120, 144, 168)
+
     start_train_date = datetime(1993, 6, 1)
     end_train_date = datetime(2020, 12, 31)
     # Short exp for debug
@@ -24,30 +26,34 @@ if __name__ == "__main__":
     # experiment_type = "cds-cmcc_oras5"
 
 
-    full_train_period_target = TimeRange(start=start_train_date, end=end_train_date, freq='MS')
-    full_train_period_input = TimeRange(start=start_train_date, end=end_train_date, freq='MS')
+    train_period = TimeRange(start=start_train_date, end=end_train_date, freq='MS')
     test_period = TimeRange(start=datetime(2021, 1, 1), end=datetime(2022, 12, 31), freq='MS')
 
+    if var_exp == "sst":
+        month_start_flag = False # SST is 6-hourly
+    else:
+        month_start_flag = True # monthly data
+
     # input has no cutoff date (only ORAS5 for consolidate vs operational datasets)
-    train_periods_input = half_train_periods_days(full_train_period_input, min_months=12, anchor="end")
+    train_periods_input = half_train_periods_days(train_period, min_months=12, anchor="end", month_start=month_start_flag)
 
     cutoff_oras5_consolidated = datetime(2014, 12, 31) # cutoff date between consolidated and operational ORAS5 datasets
     if experiment_type == "juno-cmcc_oras5":
         var_keys = [(f"{var_exp}_juno_fc", f"{var_exp}_oras5_an")]
 
-        input_provider = "ocean.juno.cmcc.hindcast.monthly"
+        input_provider = "ocean.juno.cmcc.hindcast"
         target_provider = "ocean.earthkit.oras5.reanalysis.monthly"
 
-        train_periods_target = halved_windows_split_by_cutoff(full_train_period_target, cutoff_oras5_consolidated, min_months=12, anchor="end")
+        train_periods_target = halved_windows_split_by_cutoff(train_period, cutoff_oras5_consolidated, min_months=12, anchor="end", month_start=month_start_flag)
         regrid_resolution = 0.25
 
     elif experiment_type == "juno-cmcc_juno-cmcc":
         var_keys = [(f"{var_exp}_juno_fc", f"{var_exp}_juno_an")] # _an has fixed leadtime selection, see in Scenario options
 
-        input_provider = "ocean.juno.cmcc.hindcast.monthly"
-        target_provider = "ocean.juno.cmcc.hindcast.monthly"
+        input_provider = "ocean.juno.cmcc.hindcast"
+        target_provider = "ocean.juno.cmcc.hindcast"
 
-        train_periods_target = half_train_periods_days(full_train_period_target, min_months=12, anchor="end")
+        train_periods_target = train_periods_input
         regrid_resolution = 1 # forecast model resolution
 
     elif experiment_type == "cds-cmcc_oras5":
@@ -56,12 +62,13 @@ if __name__ == "__main__":
         input_provider = "ocean.earthkit.cmcc.hindcast.monthly"
         target_provider = "ocean.earthkit.oras5.reanalysis.monthly"
 
-        train_periods_target = halved_windows_split_by_cutoff(full_train_period_target, cutoff_oras5_consolidated, min_months=12, anchor="end")
+        train_periods_target = halved_windows_split_by_cutoff(train_period, cutoff_oras5_consolidated, min_months=12, anchor="end", month_start=month_start_flag)
         regrid_resolution = 0.25
 
     else:
         raise ValueError(f"Experiment type {experiment_type} not supported.")
 
+    # Print train periods
     for i, p in enumerate(train_periods_target, 1):
         if isinstance(p, list):
             p_start = p[0].start
@@ -73,9 +80,37 @@ if __name__ == "__main__":
         months = rd.years * 12 + rd.months
         print(i, p_start.date(), "->", p_end.date(), "months:", months)
 
-    target_provider_kwargs_common = dict(
-        regrid_resolution=regrid_resolution,
-    )
+    if experiment_type == "juno-cmcc_juno-cmcc":
+        # SST is not monthly
+        if var_exp == "sst":
+            target_provider_kwargs_common = dict(
+                file_path_var_prefix="00_ocean_6hr_surface_",
+                regrid_resolution=regrid_resolution,
+            )
+
+            full_leadtimes, full_leadtime_multiple = full_leadtime_hours_sst, full_leadtime_hours_sst
+            leadtime_var_an_value = 0 # 0 hour forecast is analysis
+            leadtime_var_unit = "hours"
+            leadtime_unit = "hours"
+        else:
+            target_provider_kwargs_common = dict(
+                # file_path_var_prefix="00_ocean_mon_ocean2d_", # default
+                regrid_resolution=regrid_resolution,
+            )
+
+            full_leadtimes, full_leadtime_multiple = full_leadtimes_days, full_leadtimes_months
+            leadtime_var_an_value = 15 # 15 days forecast is analysis
+            leadtime_var_unit = "days"
+            leadtime_unit = "months"
+    else: # SST not supported
+        target_provider_kwargs_common = dict(
+                regrid_resolution=regrid_resolution,
+            )
+
+        full_leadtimes, full_leadtime_multiple = full_leadtimes_days, full_leadtimes_months
+        leadtime_var_an_value = 15 # 15 days forecast is analysis
+        leadtime_var_unit = "days"
+        leadtime_unit = "months"
 
     target_provider_kwargs_earthkit_oras5_consolidated = target_provider_kwargs_common | dict(
         earthkit_cache_dir="/work/cmcc/jd19424/.earthkit-cache",
@@ -92,8 +127,10 @@ if __name__ == "__main__":
         )
     )
 
+    input_provider_kwargs=target_provider_kwargs_common
+
     for var_fc_key, var_an_key in var_keys:
-        for leadtime_days, leadtime_months in zip(full_leadtimes_days, full_leadtimes_months):
+        for leadtime_var_fc_value, leadtime_mult in zip(full_leadtimes, full_leadtime_multiple):
             for train_p_in, train_p_tar in zip(train_periods_input, train_periods_target):
 
                 if experiment_type in ("juno-cmcc_oras5", "cds-cmcc_oras5"):
@@ -115,11 +152,11 @@ if __name__ == "__main__":
                 ocean_scenario = MLFCScenario(
                     name="ocean",
                     leadtime_var_name="leadtime",
-                    leadtime_var_fc_value=leadtime_days,
-                    leadtime_var_an_value=15, # fixed leadtime for analysis, ignored if no leadtime in analysis dataset
-                    leadtime_var_unit="days",
-                    leadtime_value=leadtime_months,
-                    leadtime_unit="months",
+                    leadtime_var_fc_value=leadtime_var_fc_value,
+                    leadtime_var_an_value=leadtime_var_an_value, # fixed leadtime for analysis, ignored if no leadtime in analysis dataset
+                    leadtime_var_unit=leadtime_var_unit, # SST -> hours, other vars -> days
+                    leadtime_value=leadtime_mult,
+                    leadtime_unit=leadtime_unit, # SST -> hours, other vars -> months
                     var_fc_key=var_fc_key,
                     var_an_key=var_an_key,
                     region_key="pacific",
@@ -133,7 +170,7 @@ if __name__ == "__main__":
                     ),
                     input_provider=input_provider,
                     target_provider=target_provider,
-                    input_provider_kwargs=dict(regrid_resolution=regrid_resolution),
+                    input_provider_kwargs=input_provider_kwargs,
                     target_provider_kwargs=target_provider_kwargs,
                     save_train=True,
                     save_test=True,
