a1a210131bd0fafeb6150c7f513f670168330435
Jacopo Dall'Aglio <fingolfin00@gmail.com>
2026-02-05 11:14:14 +0100

    calculate_metrics.py: differentiate ORAS5 and CMCC experiments
    Signed-off-by: Jacopo Dall'Aglio <fingolfin00@gmail.com>

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -79,17 +79,33 @@ if __name__ == "__main__":
         exp_root_folder = exp_root_path+"experiments_earthML_ocean/"
         analysis_folder = exp_root_path+"analysis_earthML_ocean/"
 
-        vars_dict = {
-            't14d_mse': {'exp_var': {'fc': 't14d', 'an': 't14d'}, 'region': "CentralPacific", 'exp_suffix': '32bs_50epoch_mse'},
-            'sss_mse': {'exp_var': {'fc': 'sos', 'an': 'sos'}, 'region': "CentralPacific", 'exp_suffix': '32bs_50epoch_mse'},
-            # 't17d_mse': {'exp_var': {'fc': 't14d', 'an': 'so14chgt'}, 'region': "CentralPacific", 'exp_suffix': '32bs_50epoch_mse'},
+        vars_dict_cmcc = {
+            't14d_cmcc_mse': {'exp_var': {'fc': 't14d', 'an': 't14d'}, 'region': "CentralPacific", 'exp_suffix': '32bs_50epoch_mse'},
+            'sss_cmcc_mse': {'exp_var': {'fc': 'sos', 'an': 'sos'}, 'region': "CentralPacific", 'exp_suffix': '32bs_50epoch_mse'},
+            't17d_cmcc_mse': {'exp_var': {'fc': 't17d', 'an': 't17d'}, 'region': "CentralPacific", 'exp_suffix': '32bs_50epoch_mse'},
+        }
+        vars_dict_oras5 = {
+            't14d_oras5_mse': {'exp_var': {'fc': 't14d', 'an': 'so14chgt'}, 'region': "CentralPacific", 'exp_suffix': '32bs_50epoch_mse'},
+            'sss_oras5_mse': {'exp_var': {'fc': 'sos', 'an': 'sosaline'}, 'region': "CentralPacific", 'exp_suffix': '32bs_50epoch_mse'},
+            # 't17d_cmcc_mse': {'exp_var': {'fc': 't14d', 'an': 'so14chgt'}, 'region': "CentralPacific", 'exp_suffix': '32bs_50epoch_mse'},
         }
 
-        experiments = {
+        experiments_cmcc = {
             "name": "ocean",
             "input_provider": "ocean.juno.cmcc.hindcast",
             "target_provider": "ocean.juno.cmcc.hindcast",
-            "vars": vars_dict,
+            "vars": vars_dict_cmcc,
+            "leadtimes": (1, 2, 3, 4, 5),
+            "leadtime_unit": "months",
+            "train_periods": ("19930601-20201231", "20070401-20201231", "20140201-20201231", "20170801-20201231", "20190401-20201231"),
+            "test_period": "20210101-20221231",
+            "root": exp_root_folder,
+        }
+        experiments_oras5 = {
+            "name": "ocean",
+            "input_provider": "ocean.earthkit.cmcc.hindcast.monthly",
+            "target_provider": "ocean.earthkit.oras5.reanalysis.monthly",
+            "vars": vars_dict_oras5,
             "leadtimes": (1, 2, 3, 4, 5),
             "leadtime_unit": "months",
             "train_periods": ("19930601-20201231", "20070401-20201231", "20140201-20201231", "20170801-20201231", "20190401-20201231"),
@@ -97,21 +113,45 @@ if __name__ == "__main__":
             "root": exp_root_folder,
         }
 
-    runs, _ = load_exp(
+    runs_cmcc, _ = load_exp(
         exp_root=exp_root_folder,
-        exp_cfg=experiments,
+        exp_cfg=experiments_cmcc,
+        type_data="test",
+        # type_data="train",
+    )
+    runs_oras5, _ = load_exp(
+        exp_root=exp_root_folder,
+        exp_cfg=experiments_oras5,
         type_data="test",
         # type_data="train",
     )
 
     # Calculate train period sizes
-    _, tp_sizes = load_exp(
-        exp_root=exp_root_folder,
-        exp_cfg=experiments,
-        type_data="train",
-        only_sizes=True,
-    )
+    # _, tp_sizes_cmcc = load_exp(
+    #     exp_root=exp_root_folder,
+    #     exp_cfg=experiments_cmcc,
+    #     type_data="train",
+    #     only_sizes=True,
+    # )
+    # _, tp_sizes_oras5 = load_exp(
+    #     exp_root=exp_root_folder,
+    #     exp_cfg=experiments_oras5,
+    #     type_data="train",
+    #     only_sizes=True,
+    # )
 
+    # Combine runs
+    # runs = {}
+
+    # for tp in runs_cmcc:
+    #     runs[tp] = {}
+    #     for ds_name in runs_cmcc[tp]:
+    #         ds_cmcc = runs_cmcc[tp][ds_name]
+    #         ds_oras5 = runs_oras5[tp][ds_name]
+
+    #         runs[tp][ds_name] = xr.merge([ds_cmcc, ds_oras5])
+
+    runs = runs_oras5
     runs = add_ke_to_runs(runs, suffixes=("_mse", "_het"))
 
     # TODO: refactor, check metrics.py get_runs_and_metrics
@@ -173,26 +213,6 @@ if __name__ == "__main__":
     vars_from_fc = [v for v in runs[next(iter(runs))]['fc'].data_vars if v != '_has_var']
 
     print(f"Processed vars: {vars_from_fc}")
-
-    # Plot maps in grid
-    # m = "nrmse_map"
-    # for var_name in vars_weather_from_fc:
-    #     save_path = Path(exp_root_folder_weather) / Path("plots") / Path(
-    #         f"{m}_{experiments_weather['name']}_{var_name}_{experiments_weather['test_period']}" \
-    #         f"_{experiments_weather['input_provider']}_{experiments_weather['target_provider']}.png"
-    #     )
-
-    #     plot_map_metric_grid(
-    #         metrics,
-    #         metric=m,
-    #         variable=var_name,
-    #         diff=True,
-    #         model_a="fc",
-    #         model_b="pr",
-    #         cmap="RdBu_r",
-    #         save_path=save_path,
-    #     )
-    #     print(f"Saved {m} to {save_path}")
 
     # 'mae', 'bias', 'rmse', 'stderr', 'std_of_errs', 'mape', 'smape', 'nrmse_global', 'nrmse_map_mean', 'r2_global'
     # metric_names = ["mae", "bias", "rmse", "mape", "smape", "nrmse_global", "nmae_global", "nbias_global", "abs_nbias_global", "r2_global", "corr_global", "corr_flat"]
