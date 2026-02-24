b376667fc6cb0de37522e3e75ff35f2b09a17459
Jacopo Dall'Aglio <fingolfin00@gmail.com>
2026-02-24 15:53:58 +0100

    ocean.py: all months are now available from CMCC CDS
    Signed-off-by: Jacopo Dall'Aglio <fingolfin00@gmail.com>

--- src/earthml/providers/ocean.py
+++ src/earthml/providers/ocean.py
@@ -46,7 +46,7 @@ def earthkit_cmcc_monthly_hindcast_ocean_netcdf (
     system: str = "4",
     regrid_resolution: float = 0.25,
     split_month: int = 1,
-    split_month_jump: list[str] | None = ['03', '04', '06', '07'],
+    split_month_jump: list[str] | None = None, # ['03', '04', '06', '07'],
     earthkit_cache_dir: str  = Path("/tmp/earthkit-cache/"),
     overrides: dict | None = None,
     **kw,
