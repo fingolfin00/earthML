681670c64669faf03305b75d5be0bd4fde65c127
Jacopo Dall'Aglio <fingolfin00@gmail.com>
2026-03-04 00:03:47 +0100

    new_launcher_atmo.py: add loss sweep support for multiple runs
    Signed-off-by: Jacopo Dall'Aglio <fingolfin00@gmail.com>

--- new_launcher_atmo.py
+++ new_launcher_atmo.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from earthml.dataclasses import TimeRange
 from earthml.launchers.mlfc import MLFCScenario, MLFCRunner
 from earthml.utils import half_train_periods_days
+from earthml.manager import build_loss
 
 if __name__ == "__main__":
     max_retries = 4
@@ -18,6 +19,15 @@ if __name__ == "__main__":
     # test_period = TimeRange(start=datetime(2025, 1, 1), end=datetime(2025, 1, 2), freq='12h')
     # train_periods = [full_train_period]
 
+    loss_sweep = [
+        {"loss_sel": "MSELoss"},
+        {"loss_sel": "MaskedMSELoss"},
+        {"loss_sel": "VarianceNormalizedMSELoss", "variance_type": "spatial", "latitudes": False}, # channel, geochannel, spatial, temporal, geotemporal
+        {"loss_sel": "VarianceNormalizedMSELoss", "variance_type": "geochannel", "latitudes": True},
+        {"loss_sel": "HeteroBiasCorrectionLoss", "use_first_input": True, "variance_type": "spatial", "latitudes": False},
+        {"loss_sel": "GaussianNLLFromLogits"},
+    ]
+
     root_path_forecast     = "/data/inputs/METOCEAN/rolling/model/atmos/ECMWF/IFS_010/1.0forecast/1h/grib/"
     root_path_forecast_new = "/data/inputs/METOCEAN/rolling/model/atmos/ECMWF/IFS_010_new/1.0forecast/1h/grib/"
     root_path_analysis     = "/data/inputs/METOCEAN/historical/model/atmos/ECMWF/IFS_010/analysis/6h/grib/"
@@ -26,78 +36,68 @@ if __name__ == "__main__":
     for i, p in enumerate(train_periods, 1):
         print(i, p.start.date(), "->", p.end.date(), "days:", (p.end - p.start).days)
 
-    for var_key in var_keys:
-        for leadtime_days in full_leadtimes:
-            leadtime_hours = int(leadtime_days * 24)
-            for train_p in train_periods:
-                weather_scenario = MLFCScenario(
-                    name="weather",
-                    leadtime_var_fc_name="leadtime",
-                    leadtime_var_an_name="leadtime",
-                    leadtime_var_fc_value=leadtime_hours,
-                    leadtime_var_an_value=leadtime_hours,
-                    leadtime_var_unit="hours",
-                    leadtime_value=leadtime_hours,
-                    leadtime_unit="hours",
-                    var_fc_key=var_key,
-                    var_an_key=var_key,
-                    region_key="conus",
-                    train_period=train_p,
-                    test_period=dict(
-                        input= [test_period, test_period_new],
-                        target=[test_period, test_period_new],
-                    ),
-                    input_provider= "atmo.juno.ecmwf.forecast.hourly",
-                    target_provider="atmo.juno.ecmwf.analysis.6hourly",
-                    input_provider_kwargs=dict(
-                        train=dict(root_path=root_path_forecast),
-                        test=[dict(root_path=root_path_forecast), dict(root_path=root_path_forecast_new, file_header="CMS")],
-                    ),
-                    target_provider_kwargs=dict(
-                        train=dict(root_path=root_path_analysis),
-                        test=[dict(root_path=root_path_analysis), dict(root_path=root_path_analysis_new, file_header="CMD")],
-                    ),
-                    save_train=True,
-                    save_test=True,
-                    torch_preprocess_fn=None,
-                )
+    for loss_cfg in loss_sweep:
+        loss_name, loss_params, loss_suf = build_loss(loss_cfg)
+        for var_key in var_keys:
+            for leadtime_days in full_leadtimes:
+                leadtime_hours = int(leadtime_days * 24)
+                for train_p in train_periods:
+                    weather_scenario = MLFCScenario(
+                        name="weather",
+                        leadtime_var_fc_name="leadtime",
+                        leadtime_var_an_name="leadtime",
+                        leadtime_var_fc_value=leadtime_hours,
+                        leadtime_var_an_value=leadtime_hours,
+                        leadtime_var_unit="hours",
+                        leadtime_value=leadtime_hours,
+                        leadtime_unit="hours",
+                        var_fc_key=var_key,
+                        var_an_key=var_key,
+                        region_key="conus",
+                        train_period=train_p,
+                        test_period=dict(
+                            input= [test_period, test_period_new],
+                            target=[test_period, test_period_new],
+                        ),
+                        input_provider= "atmo.juno.ecmwf.forecast.hourly",
+                        target_provider="atmo.juno.ecmwf.analysis.6hourly",
+                        input_provider_kwargs=dict(
+                            train=dict(root_path=root_path_forecast),
+                            test=[dict(root_path=root_path_forecast), dict(root_path=root_path_forecast_new, file_header="CMS")],
+                        ),
+                        target_provider_kwargs=dict(
+                            train=dict(root_path=root_path_analysis),
+                            test=[dict(root_path=root_path_analysis), dict(root_path=root_path_analysis_new, file_header="CMD")],
+                        ),
+                        save_train=True,
+                        save_test=True,
+                        torch_preprocess_fn=None,
+                    )
 
-                runner = MLFCRunner(
-                    scenario=weather_scenario,
-                    exp_root_folder="/work/cmcc/jd19424/test-ML/experiments_earthML_weather/",
-                    exp_suffix="_32bs_50epoch_mse",
-                    # exp_suffix="_32bs_50epoch_gnll",
-                    # exp_suffix="_32bs_50epoch_het",
-                    # ML options
-                    learning_rate=1e-3,
-                    batch_size=32,
-                    epochs=50,
-                    loss="MSELoss",
-                    # loss="earthml.losses.GaussianNLLFromLogits",
-                    # loss="earthml.losses.HeteroBiasCorrectionLoss",
-                    # loss_params=dict(
-                    #     # net=dict(use_first_input=False), # gnll
-                    #     net=dict(use_first_input=True),
-                    #     loss=dict(
-                    #         # reduction="mean", # gnll
-                    #         lambda_identity=0.1,
-                    #         bias_scale=0.5,
-                    #         eps=1e-6)
-                    # ),
-                    accumulate_grad_batches=2,
-                    earlystopping_patience=30,
-                )
+                    runner = MLFCRunner(
+                        scenario=weather_scenario,
+                        exp_root_folder="/work/cmcc/jd19424/test-ML/experiments_earthML_weather/",
+                        exp_suffix=f"_{loss_suf}",
+                        # ML options
+                        learning_rate=1e-3,
+                        batch_size=32,
+                        epochs=50,
+                        loss=loss_name,
+                        loss_params=loss_params,
+                        accumulate_grad_batches=2,
+                        earlystopping_patience=30,
+                    )
 
-                success = False
+                    success = False
 
-                for _ in range(max_retries):
-                    try:
-                        runner.run(mode="train_test")
-                        success = True
-                        break
-                    except (RuntimeError, OSError) as e:
-                        print(e)
-                        pass
+                    for _ in range(max_retries):
+                        try:
+                            runner.run(mode="train_test")
+                            success = True
+                            break
+                        except (RuntimeError, OSError) as e:
+                            print(e)
+                            pass
 
-                if not success:
-                    continue
+                    if not success:
+                        continue
