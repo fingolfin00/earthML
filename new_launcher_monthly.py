dcf6066fa81ec531f7a953a1e3dbca01ffc01dc0
Jacopo Dall'Aglio <fingolfin00@gmail.com>
2026-03-27 03:56:45 +0100

    new_launcher_monthly.py: update
    Signed-off-by: Jacopo Dall'Aglio <fingolfin00@gmail.com>

--- new_launcher_monthly.py
+++ new_launcher_monthly.py
@@ -16,25 +16,25 @@ if __name__ == "__main__":
     max_retries                 = 10                        # n attempts in case of errors in inner loop
     run_mode                    = "train_test_on_train"     # train_test_on_train, train_test, dryrun, train, test
     host_machine                = "local"                   # local, juno
-    experiment_mode             = "short"                   # full, short, debug
+    experiment_mode             = "full"                   # full, short, debug
     only_longest_train_period   = True                      # only train on the largest train period (if False, train on all periods, which can be much slower but allows to see variability across train periods)
     add_hyper_exp_name_suffix   = False                     # if True use also batch_size, max_epochs, initial_learning_rate in exp name (and resulting folder) automatically extending exp_suffix
-    extra_exp_suffix            = ""                        # additional custom suffix to add to exp name (and resulting folder), e.g. "_debug" or "_try1"
+    extra_exp_suffix            = ""                # additional custom suffix to add to exp name (and resulting folder), e.g. "_debug" or "_try1"
 
-    exp_root_folder             = "/Users/jacopodallaglio/ML/experiments_earthML_ocean/"
-    earthkit_cache_dir          = "/Users/jacopodallaglio/ML/.earthkit-cache"   # if using earthkit datasource
+    exp_root_folder             = "/Users/jacopodallaglio/ML/experiments_earthML_ocean"
+    earthkit_cache_dir          = "/Users/jacopodallaglio/ML/.earthkit-cache"       # if using earthkit datasource
 
-    vars_sweep                  = ["sst", "ssh", "sss", "t14d", "t17d"] # e.g. ["sst", "ssh", "sss", "t14d", "t17d"]
-    regions_sweep               = ["pacific"]                           # e.g. ["pacific", "atlantic", "indian"]
+    vars_sweep                  = ["sst"]             # e.g. ["sst", "ssh", "sss", "t14d", "t17d"]
+    regions_sweep               = ["natlantic"]                               # e.g. ["pacific", "natlantic", "indian"]
 
-    inpaint_nan                 = False                     # whether to inpaint nan values in input and target datasets (after loading, before torch dataset generation)
-    anomaly                     = True                      # if True, predict anomaly (i.e. remove climatology from target variable), otherwise predict absolute values
+    inpaint_nan                 = True                      # whether to inpaint nan values in input and target datasets (after loading, before torch dataset generation)
+    anomaly                     = False                      # if True, predict anomaly (i.e. remove climatology from target variable), otherwise predict absolute values
     per_epoch_resplit           = False                     # if True, split test and validation randomly per epoch with a different seed, if False only one initial split for all epochs (fixed seed)
 
-    start_train_date            = datetime(1993, 7, 1)
-    end_train_date              = datetime(2020, 12, 31)
+    start_train_date            = datetime(1993, 6, 1)
+    end_train_date              = datetime(2020, 12, 1)
     start_test_date             = datetime(2021, 1, 1)
-    end_test_date               = datetime(2022, 12, 31)
+    end_test_date               = datetime(2022, 12, 1)
 
     target_realization_avg      = False                     # average over realizations for target variable (if True, add _taravg suffix to exp_suffix)
     realization_as_channel      = False                     # use realization a channel dim (if True, add _rasc suffix to exp_suffix)
@@ -57,8 +57,8 @@ if __name__ == "__main__":
         ]
     else:
         loss_sweep = [
-            {"loss_sel": "MSELoss"},
-            # {"loss_sel": "GeoMSELoss", "latitudes": True},
+            # {"loss_sel": "MSELoss"},
+            {"loss_sel": "GeoMSELoss", "latitudes": True},
             # {"loss_sel": "MaskedMSELoss"},
             # {"loss_sel": "GeoMaskedMSELoss", "latitudes": True},
             # {"loss_sel": "VarNormMaskMSELoss", "variance_type": "spatial", "latitudes": False}, # channel, geochannel, spatial, temporal, geotemporal
@@ -73,12 +73,15 @@ if __name__ == "__main__":
     if experiment_mode in ("short", "debug"):
         # Short exp
         full_leadtimes_days         = (165,)
+        # full_leadtimes_days         = (135,) # fake 6 just to compare
         full_leadtimes_atmo_days    = (180,)
+        # full_leadtimes_atmo_days    = (150,) # fake 6 just to compare
         full_leadtimes_months       = (6,)
         if experiment_mode == "debug":
             # Very short periods for debug
-            start_train_date        = datetime(1993, 7, 1)
-            end_train_date          = datetime(1994, 12, 31)
+            start_train_date        = datetime(1993, 6, 1)
+            # end_train_date          = datetime(1994, 12, 31)
+            end_train_date          = datetime(1993, 7, 31)
             start_test_date         = datetime(2021, 1, 1)
             end_test_date           = datetime(2021, 12, 31)
     else:
@@ -92,7 +95,7 @@ if __name__ == "__main__":
     test_period     = TimeRange(start=start_test_date,  end=end_test_date,  freq='MS')
 
     # Set var categories
-    vars_cloud_oras5        = ("sst", "ssh")
+    vars_cloud_oras5        = ("sst", "ssh") # only for juno experiments
     vars_cloud_cds_atmo     = ("sst",)
     vars_cloud_cds_ocean    = ("ssh",)
 
