e743e0a0eb5290838ceff6254d7b4e95af5a8ac4
Jacopo Dall'Aglio <fingolfin00@gmail.com>
2026-04-07 23:24:21 +0200

    sources: expose new source copernicusmarine
    Also refactor source management.
    
    Signed-off-by: Jacopo Dall'Aglio <fingolfin00@gmail.com>

--- src/earthml/sources/__init__.py
+++ src/earthml/sources/__init__.py
@@ -1,20 +1,33 @@
 from .base import BaseSource
+from .dataclasses import SourceConfig, RegridConfig, DataSource
 from .registry import get_source_class, build_source
+from .registry import _SOURCES as _REGISTRY_SOURCES
+
+from .earthkit import EarthkitSourceConfig
+from .juno_local import JunoLocalSourceConfig, JunoLocalSourceFileNameConfig
+from .xarray_local import XarrayLocalSourceConfig
+from .copernicusmarine import CopernicusmarineSourceConfig
 
 
 __all__ = [
     "BaseSource",
     "get_source_class",
     "build_source",
+    "SourceConfig",
+    "RegridConfig",
+    "DataSource",
+    "EarthkitSourceConfig",
+    "JunoLocalSourceConfig",
+    "JunoLocalSourceFileNameConfig",
+    "XarrayLocalSourceConfig",
+    "CopernicusmarineSourceConfig",
 ]
 
 
-_SOURCES = {
-    "SumSource": (".combinators", "SumSource"),
-    "XarrayLocalSource": (".xarray_local", "XarrayLocalSource"),
-    "JunoLocalSource": (".juno_local", "JunoLocalSource"),
-    "EarthkitSource": (".earthkit", "EarthkitSource"),
-}
+_SOURCES = {"SumSource": (".combinators", "SumSource")}
+for target in _REGISTRY_SOURCES.values():
+    module_path, class_name = target.split(":")
+    _SOURCES[class_name] = (f".{module_path.rsplit('.', 1)[1]}", class_name)
 
 def __getattr__(name: str):
     if name in _SOURCES:
