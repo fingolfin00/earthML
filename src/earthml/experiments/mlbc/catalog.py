0d3a68037771ac67058d383490ca50358301a5cc
Jacopo Dall'Aglio <fingolfin00@gmail.com>
2026-04-07 23:16:19 +0200

    catalog.py: add full Europe region
    Signed-off-by: Jacopo Dall'Aglio <fingolfin00@gmail.com>

--- src/earthml/experiments/mlbc/catalog.py
+++ src/earthml/experiments/mlbc/catalog.py
@@ -88,6 +88,7 @@ def make_region ():
         westconus=Region(name="WestConUS", lon=(-130, -90), lat=(45, 30)),
         conus=Region(name="ConUS", lon=(-130, -60), lat=(50, 25)),
         westeurope=Region(name="WestEurope", lon=(-10, 36), lat=(55, 35)),
+        europe=Region(name="Europe", lon=(-30, 60), lat=(80, 30)),
         italy=Region(name="ItalianPeninsula", lon=(5, 23.5), lat=(49, 25.5)),
         pacific=Region(name="CentralPacific", lon=(-200, -120), lat=(30, -30)),
         natlantic=Region(name="NorthAtlantic", lon=(-100, 40), lat=(80, 0)),
