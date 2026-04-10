08164bbc44f1bcd7bff4a281dfc3e983664f5803
Jacopo Dall'Aglio <fingolfin00@gmail.com>
2026-04-10 14:00:09 +0200

    sources: juno_local: add get_data outer loop progress bar
    Signed-off-by: Jacopo Dall'Aglio <fingolfin00@gmail.com>

--- src/earthml/sources/juno_local.py
+++ src/earthml/sources/juno_local.py
@@ -15,6 +15,14 @@ import xarray as xr
 from dask.utils import SerializableLock
 
 from rich import print
+from rich.progress import (
+    Progress,
+    SpinnerColumn,
+    TextColumn,
+    BarColumn,
+    TimeElapsedColumn,
+    MofNCompleteColumn,
+)
 
 from .dataclasses import DataSource, Sample, RegridConfig
 from .utils import retry_fetch_after_hdf_err
@@ -196,80 +204,90 @@ class JunoLocalSource(MFXarrayLocalSource):
 
         realization_concat_dim = common_args["concat_dim"]
 
-        for sample, date in zip(samples, dates):
-            assert isinstance(sample, list), f"Sample should be a list but it is {type(sample)}"
+        with Progress(
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            MofNCompleteColumn(),
+            TimeElapsedColumn(),
+        ) as prog:
+            task = prog.add_task("Opening samples...", total=len(samples))
+            for sample, date in zip(samples, dates):
+                assert isinstance(sample, list), f"Sample should be a list but it is {type(sample)}"
 
-            args = dict(common_args) # make local copy
+                args = dict(common_args) # make local copy
 
-            # Try sorting by realization "...rxx..."
-            if len(sample) > 1:
-                def _realization_key(p: Path) -> int:
-                    m = re.search(r"_r(\d+)", p.name)
-                    if not m:
-                        raise ValueError(f"Could not parse realization from filename: {p.name}")
-                    return int(m.group(1))
+                # Try sorting by realization "...rxx..."
+                if len(sample) > 1:
+                    def _realization_key(p: Path) -> int:
+                        m = re.search(r"_r(\d+)", p.name)
+                        if not m:
+                            raise ValueError(f"Could not parse realization from filename: {p.name}")
+                        return int(m.group(1))
 
-                sample = sorted(sample, key=_realization_key)
-            # print(sample)
-            args['paths'] = sample
+                    sample = sorted(sample, key=_realization_key)
+                # print(sample)
+                args['paths'] = sample
 
-            if isinstance(self.data_selection.variable, list):
-                var_ds_list = []
-                for var in self.data_selection.variable:
-                    var_args = dict(args) # again local copy to avoid leakage between iterations
+                if isinstance(self.data_selection.variable, list):
+                    var_ds_list = []
+                    for var in self.data_selection.variable:
+                        var_args = dict(args) # again local copy to avoid leakage between iterations
 
+                        if self.engine == "cfgrib":
+                            var_args["backend_kwargs"] = {
+                                "indexpath": "",
+                                "filter_by_keys": {"cfVarName": var.name},
+                            }
+                            # var_args["backend_kwargs"] = {"filter_by_keys": {"cfVarName": var.name}} # not currently possible to filter with a list of keys (see https://github.com/ecmwf/cfgrib/issues/138)
+                            # var_args["indexpath"] = ""
+
+                        var_args["preprocess"] = partial(
+                            preprocess_mfdataset,
+                            data=self.data_selection,
+                            var_name=var.name,
+                            date=date,
+                        )
+
+                        def _open_mfdataset():
+                            return xr.open_mfdataset(**var_args)
+
+                        var_ds_list.append(retry_fetch_after_hdf_err(_open_mfdataset, error_re=r"Unspecified error in H5DSget_num_scales.*"))
+
+                    ds_sample = xr.merge(var_ds_list, compat="no_conflicts", combine_attrs="no_conflicts")
+
+                    # Load realization coord
+                    if realization_concat_dim in ds_sample.coords:
+                        ds_sample = ds_sample.assign_coords({realization_concat_dim: ds_sample[realization_concat_dim].load()})
+                else:
                     if self.engine == "cfgrib":
-                        var_args["backend_kwargs"] = {
-                            "indexpath": "",
-                            "filter_by_keys": {"cfVarName": var.name},
+                        args["backend_kwargs"] = {
+                            "indexpath": "",                 # disable .idx writing
+                            "filter_by_keys": {"cfVarName": self.data_selection.variable.name},
                         }
-                        # var_args["backend_kwargs"] = {"filter_by_keys": {"cfVarName": var.name}} # not currently possible to filter with a list of keys (see https://github.com/ecmwf/cfgrib/issues/138)
-                        # var_args["indexpath"] = ""
+                        # args["backend_kwargs"] = {"filter_by_keys": {"cfVarName": self.data_selection.variable.name}}
+                        # args["indexpath"] = ""
 
-                    var_args["preprocess"] = partial(
-                        preprocess_mfdataset,
-                        data=self.data_selection,
-                        var_name=var.name,
-                        date=date,
-                    )
+                    args["preprocess"] = partial(preprocess_mfdataset, data=self.data_selection, var_name=self.data_selection.variable.name, date=date)
 
-                    def _open_mfdataset():
-                        return xr.open_mfdataset(**var_args)
+                    # Tested support for netcdf4
+                    def _open_mfdataset ():
+                        return xr.open_mfdataset(**args)
 
-                    var_ds_list.append(retry_fetch_after_hdf_err(_open_mfdataset, error_re=r"Unspecified error in H5DSget_num_scales.*"))
+                    ds_sample = retry_fetch_after_hdf_err(_open_mfdataset, error_re=r"Unspecified error in H5DSget_num_scales.*")
 
-                ds_sample = xr.merge(var_ds_list, compat="no_conflicts", combine_attrs="no_conflicts")
+                    # Load realization coord
+                    if realization_concat_dim in ds_sample.coords:
+                        ds_sample = ds_sample.assign_coords({realization_concat_dim: ds_sample[realization_concat_dim].load()})
+                    # Promote realization to coord
+                    if realization_concat_dim in ds_sample.dims:
+                        ds_sample = ds_sample.assign_coords({realization_concat_dim: ds_sample[realization_concat_dim]})
 
-                # Load realization coord
-                if realization_concat_dim in ds_sample.coords:
-                    ds_sample = ds_sample.assign_coords({realization_concat_dim: ds_sample[realization_concat_dim].load()})
-            else:
-                if self.engine == "cfgrib":
-                    args["backend_kwargs"] = {
-                        "indexpath": "",                 # disable .idx writing
-                        "filter_by_keys": {"cfVarName": self.data_selection.variable.name},
-                    }
-                    # args["backend_kwargs"] = {"filter_by_keys": {"cfVarName": self.data_selection.variable.name}}
-                    # args["indexpath"] = ""
+                # print(ds_sample)
+                # ds_sample is alway Rx1xYxX
+                samples_d[date] = ds_sample
 
-                args["preprocess"] = partial(preprocess_mfdataset, data=self.data_selection, var_name=self.data_selection.variable.name, date=date)
-
-                # Tested support for netcdf4
-                def _open_mfdataset ():
-                    return xr.open_mfdataset(**args)
-
-                ds_sample = retry_fetch_after_hdf_err(_open_mfdataset, error_re=r"Unspecified error in H5DSget_num_scales.*")
-
-                # Load realization coord
-                if realization_concat_dim in ds_sample.coords:
-                    ds_sample = ds_sample.assign_coords({realization_concat_dim: ds_sample[realization_concat_dim].load()})
-                # Promote realization to coord
-                if realization_concat_dim in ds_sample.dims:
-                    ds_sample = ds_sample.assign_coords({realization_concat_dim: ds_sample[realization_concat_dim]})
-
-            # print(ds_sample)
-            # ds_sample is alway Rx1xYxX
-            samples_d[date] = ds_sample
+                prog.advance(task)
 
         # Count valid realizations
         samples_len, missing_samples = [], []
