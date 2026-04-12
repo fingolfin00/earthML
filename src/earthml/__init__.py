8061cfdc307793fa8843e3bca1059f1f3b7db5cf
Jacopo Dall'Aglio <fingolfin00@gmail.com>
2026-04-12 23:22:49 +0200

    Use importlib to expose global modules and methods
    This is to avoid earthkit-data is loaded everywhere.
    
    Signed-off-by: Jacopo Dall'Aglio <fingolfin00@gmail.com>

--- src/earthml/__init__.py
+++ src/earthml/__init__.py
@@ -1,18 +1,7 @@
 from importlib.metadata import version as _version
+import importlib
 
 __version__ = _version("earthml")
-
-from .base import DataSelection, TimeRange, Leadtime
-from .sources import DataSource
-from .misc import Dask, Table
-
-from .neural.losses import build_loss
-
-from .experiments.mlbc import MLBCNeuralNet, MLBCExperimentLauncherConfig, MLBCExperimentLauncher, load_exp, load_all_exp_from_folder
-
-from .metrics import get_runs_and_metrics, metrics_to_df
-
-from .accessors.earthml import EarthMLAccessor as _EarthMLAccessor # triggers accessor __init__
 
 __all__ = [
     "__version__",
@@ -36,3 +25,40 @@ __all__ = [
     "get_runs_and_metrics",
     "metrics_to_df",
 ]
+
+_EXPORTS = {
+    "DataSelection": (".base", "DataSelection"),
+    "TimeRange": (".base", "TimeRange"),
+    "Leadtime": (".base", "Leadtime"),
+    "DataSource": (".sources", "DataSource"),
+    "Dask": (".misc", "Dask"),
+    "Table": (".misc", "Table"),
+    "build_loss": (".neural.losses", "build_loss"),
+    "MLBCNeuralNet": (".experiments.mlbc", "MLBCNeuralNet"),
+    "MLBCExperimentLauncherConfig": (".experiments.mlbc", "MLBCExperimentLauncherConfig"),
+    "MLBCExperimentLauncher": (".experiments.mlbc", "MLBCExperimentLauncher"),
+    "load_exp": (".experiments.mlbc", "load_exp"),
+    "load_all_exp_from_folder": (".experiments.mlbc", "load_all_exp_from_folder"),
+    "get_runs_and_metrics": (".metrics", "get_runs_and_metrics"),
+    "metrics_to_df": (".metrics", "metrics_to_df"),
+}
+
+
+def __getattr__(name: str):
+    if name == "_EarthMLAccessor":
+        module = importlib.import_module(".accessors.earthml", __name__)
+        value = getattr(module, "EarthMLAccessor")
+        globals()[name] = value
+        return value
+
+    if name in _EXPORTS:
+        module_name, attr = _EXPORTS[name]
+        module = importlib.import_module(module_name, __name__)
+        value = getattr(module, attr)
+        globals()[name] = value
+        return value
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+
+importlib.import_module(".accessors.earthml", __name__)
