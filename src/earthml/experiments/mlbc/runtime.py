a59d215d60a81adafe39a3c43bd0ece6c6d0eaeb
Jacopo Dall'Aglio <fingolfin00@gmail.com>
2026-04-17 22:16:27 +0200

    experiments: mlbc: allow pass logging level
    Signed-off-by: Jacopo Dall'Aglio <fingolfin00@gmail.com>

--- src/earthml/experiments/mlbc/runtime.py
+++ src/earthml/experiments/mlbc/runtime.py
@@ -1,17 +1,18 @@
 import os, logging, warnings
 from pathlib import Path
+from typing import Literal
 from dataclasses import dataclass
 
 from ...misc import Dask
-from ...logging import configure_logging, get_logger
+from ...logging import configure_logging, get_logger, LoggingConfig
 
-def configure_warnings_and_logging() -> None:
+def configure_warnings_and_logging(log_config: LoggingConfig) -> None:
     warnings.filterwarnings("ignore", category=FutureWarning)
     warnings.filterwarnings("ignore", category=UserWarning)
     logging.getLogger("distributed.scheduler").setLevel(logging.ERROR)
     logging.getLogger("distributed.worker").setLevel(logging.ERROR)
     logging.getLogger("distributed.nanny").setLevel(logging.ERROR)
-    configure_logging()
+    configure_logging(log_config)
 
 def configure_ca_bundle() -> None:
     bundle = Path.home() / "certs" / "earthml-ca-bundle.pem"
@@ -26,9 +27,11 @@ class Runtime:
     memory_limit: str = "auto"
     nanny: bool = True
     needs_ca_bundle: bool = False
+    log_level: Literal["info", "debug"] = "info"
 
     def start(self) -> Dask:
-        configure_warnings_and_logging()
+        log_config = LoggingConfig(level=self.log_level)
+        configure_warnings_and_logging(log_config)
         if self.needs_ca_bundle:
             configure_ca_bundle()
         d = Dask(
