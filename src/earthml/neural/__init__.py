9c5e340e9c04272537fb560539d856ddecb6ddaf
Jacopo Dall'Aglio <fingolfin00@gmail.com>
2026-09-24 10:43:35 +0200

    neural: __init__: expose all new metrics and diagnostics utils
    Signed-off-by: Jacopo Dall'Aglio <fingolfin00@gmail.com>

--- src/earthml/neural/__init__.py
+++ src/earthml/neural/__init__.py
@@ -1,23 +1,57 @@
-from .module import EarthMLLightningModule, SplitDataModule, SingleMonthBatchSampler, SplitStrategy
+from .module import (
+    RichEarlyStopping,
+    LearningRateChangePrinter,
+    EarthMLLightningModule,
+    SplitDataModule,
+    SingleMonthBatchSampler,
+    SplitStrategy,
+)
 from .dataset import XarrayDataset, XarraySubset
-from .metrics import MaskedMAE, MaskedRMSE, MaskedSpatialCorr
+from .metrics import (
+    MaskedBias,
+    MaskedMAE,
+    MaskedMSE,
+    MaskedRMSE,
+    MaskedSpatialCorr,
+    MaskedTemporalCorr,
+    MaskedStdRatio,
+)
+from .diagnostics import diagnostics
 from .normalize import Normalize, MonthlyNormalize, NormalizationMode
 from .utils import call_loss, resolve_loss
+from .losses import build_loss
+from .nets import build_net
 
 __all__ = [
+    # Callbacks
+    "RichEarlyStopping",
+    "LearningRateChangePrinter",
+    # Modules
     "EarthMLLightningModule",
     "SplitDataModule",
     "SingleMonthBatchSampler",
     "SplitStrategy",
+    # Torch datasets
     "XarrayDataset",
     "XarraySubset",
+    # TorchMetrics
+    "MaskedBias",
     "MaskedMAE",
+    "MaskedMSE",
     "MaskedRMSE",
     "MaskedSpatialCorr",
+    "MaskedTemporalCorr",
+    "MaskedStdRatio",
+    # Diagnostics
+    "diagnostics",
     # Normalizers
     "Normalize",
     "MonthlyNormalize",
     "NormalizationMode",
+    # Loss
     "call_loss",
     "resolve_loss",
+    "build_loss",
+    # Net
+    "build_net",
 ]
